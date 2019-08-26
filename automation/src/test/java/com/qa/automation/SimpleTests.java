ca97adc5e4e90112a3e6e31ee9b1c252b070e0a0
arunkanthan <40499490+arunkanthan@users.noreply.github.com>
2019-08-26 18:01:55 +0530

    initital commit
    initial commit

---
+++ automation/src/test/java/com/qa/automation/SimpleTests.java
@@ -0,0 +1,25 @@
+package com.qa.automation;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SimpleTests {
+	
+	
+	@Test
+	public void test1() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void test2() {
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void test3() {
+		Assert.assertTrue(true);
+	}
+
+
+}
